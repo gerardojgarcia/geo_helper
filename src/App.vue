db9aa350d16f34b3181b1525e217f9ab6e7f682d
gerardojgarcia <gerardojgarcia@pm.me>
2022-09-01 10:50:14 -0700

    first commit'

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script >
+
+import AppButton from './components/AppButton.vue'
+
+export default {
+
+components: {
+  AppButton,
+},
+props: {
+  name: String
+}
+
+
+}
+
+
+
+
+</script>
+
+<template>
+
+
+<div>
+
+  <AppButton name="Login/Register" />
+
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppButton.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppButton',
+
+    props: {
+        name: String,
+    }
+}
+</script>
+
+<template>
+    <div>
+        <button class="text-white rounded-sm bg-teal-500 shadow-lg px-4 py-1">{{name}}</button>
+        
+    </div>
+
+</template>
